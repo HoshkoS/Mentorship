14ded72c235a42e6a16d0b5bb6565d7b792c91eb
HoshkoS <sofochka9500ho@gmail.com>
2024-11-14 22:51:41 +0200

    Add axios and first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import api from './utils/axios';
 
 const App = () => {
-  return <h1>Hello World!</h1>;
+  const [message, setMessage] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchGreeting = async () => {
+      try {
+        const response = await api.get('/greetings');
+        setMessage(response.data);
+      } catch (error) {
+        setError('Error fetching greeting');
+      }
+    };
+
+    fetchGreeting();
+  }, []);
+
+  return (
+    <div>
+      <h1>Greeting App</h1>
+      {error && <p>{error}</p>}
+      {message ? (
+        <p>{message}</p>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default App;
